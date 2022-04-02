06927780826a0ddd05a6aaa48315a69a7c7b8a53
md2eoseo <md2eoseo@gmail.com>
2022-04-02 16:20:58 +0900

    clean boilerplate

---
+++ pages/todo.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <h1>Todo</h1>
+      <v-list>
+        <v-list-item v-for="(todo, i) in todos" :key="i">
+          <div>{{ todo.title }}</div>
+          <div>{{ todo.date }}</div>
+        </v-list-item>
+      </v-list>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "TodoPage",
+  setup(props, context) {
+    // Attributes (Non-reactive object)
+    console.log(context.attrs);
+
+    // Slots (Non-reactive object)
+    console.log(context.slots);
+
+    // Emit Events (Method)
+    console.log(context.emit);
+  },
+  mounted(ctx) {
+    console.log(ctx);
+  },
+  data() {
+    return {
+      todos: [
+        { id: 1, title: "Hello", date: "2022-03-25" },
+        { id: 2, title: "World", date: "2022-03-25" },
+        { id: 3, title: "Nuxt", date: "2022-03-25" },
+      ],
+    };
+  },
+  methods: {
+    hello(ctx) {
+      console.log(ctx);
+    },
+  },
+};
+</script>
